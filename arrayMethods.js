2d15901af090acbba8745a0aa35a5b0a7c5691e3
Slobodan Krasavcevic <54939781+BobanBg79@users.noreply.github.com>
2021-04-03 14:16:44 +0200

    Create arrayMethods.js

---
+++ arrayMethods.js
@@ -0,0 +1,19 @@
+// TASK: find all occurances of selected item inside an array;
+var arr = [1,2,5,7,8,1,6,9,8,7,5,1,6,5,8,9,7,8];
+
+var func = (arr, item) => {
+  const positions = [];
+  debugger;
+  function runSearch(arr, item, fromIndex = 0){
+    const index = arr.indexOf(item, fromIndex);
+    if (index >= 0){
+      positions.push(index);
+      if(index < arr.length - 1) runSearch(arr, item, index + 1);
+    }
+  }
+  runSearch(arr, item);
+  console.log('positions: ', positions);
+  return positions;
+}
+
+func(arr, 1);
