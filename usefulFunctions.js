a8a01ee4d3add2c1db463a518ee3e3e70d1c6759
Slobodan Krasavcevic <54939781+BobanBg79@users.noreply.github.com>
2021-10-30 21:35:49 +0200

    add debounce in file

--- usefulFunctions.js
+++ usefulFunctions.js
@@ -4,3 +4,16 @@ function triangleAreaCalc(a,b,c){
       var area = Math.pow(calc, 0.5);
       return area;
 }
+
+
+// *** Debounce function Regular Version ***
+// const myDebounce = (callback, delay) => {
+//     let timerId;
+//     function x(args){
+//         clearTimeout(timerId);
+//         timerId = setTimeout(() => callback(args), delay)
+//     }
+//     return x;
+// };
+
+// const y = myDebounce((args) => console.log(args), 2000);
